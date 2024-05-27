b63ae86e34a2b06a29b0fd1acd0b0318a7f4cba0
Ayman Salah <aymancoom3@gmail.com>
2024-05-27 05:05:46 +0300

    Filter and Search Data

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,20 +7,32 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
-use Laravie\Parser\Xml\Reader;
-use Laravie\Parser\Xml\Document;
-
 use App\Entity\Product;
+use Knp\Component\Pager\PaginatorInterface;
 
 class ProductController extends AbstractController
 {
     #[Route('/products', name: 'show_products')]
-    public function importForm(Request $request, EntityManagerInterface $entityManager): Response
+    public function showProducts(Request $request, EntityManagerInterface $entityManager, PaginatorInterface $paginator): Response
     {
-        $products = $entityManager->getRepository(Product::class)->findAll();
+        $searchTerm = $request->query->get('search', '');
+        $queryBuilder = $entityManager->getRepository(Product::class)->createQueryBuilder('p');
+
+        if ($searchTerm) {
+            $queryBuilder->where('p.name LIKE :searchTerm OR p.description LIKE :searchTerm')
+                ->setParameter('searchTerm', '%' . $searchTerm . '%');
+        }
+
+        $query = $queryBuilder->getQuery();
+
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            10
+        );
 
         return $this->render('show_products.html.twig', [
-            'products' => $products,
+            'pagination' => $pagination,
         ]);
     }
 }
